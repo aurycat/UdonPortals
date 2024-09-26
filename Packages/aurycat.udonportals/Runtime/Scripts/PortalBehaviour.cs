b92d7237366dbd16c75ece94f7a23bab5dba9be7
Superbstingray <Superbstingraynz@gmail.com>
2024-09-27 04:19:09 +1200

    Accurate Stereo Separation calculation
    Update to the stereo separation calculation to make it accurate to VRChat's separation & offset

--- Packages/aurycat.udonportals/Runtime/Scripts/PortalBehaviour.cs
+++ Packages/aurycat.udonportals/Runtime/Scripts/PortalBehaviour.cs
@@ -809,15 +809,15 @@ public class PortalBehaviour : UdonSharpBehaviour
 
 			// Calculate stereo separation. This changes based on avatar size.
 			if (stereoSeparationMode == 0) {
-				_stereoSeparation = (1f/trackingScale.localScale.x) * dummyCameraL.stereoSeparation;
+				_stereoSeparation = 0.03f * (1f / trackingScale.localScale.x);
 			} else if (stereoSeparationMode == 1) {
-				_stereoSeparation = trackingScale.localScale.x * dummyCameraL.stereoSeparation;
+				_stereoSeparation = 0.03f * trackingScale.localScale.x;
 			} else {
 				_stereoSeparation = manualStereoSeparation;
 			}
-			float centerToEyeDist = _stereoSeparation * 0.5f;
-			offsetL.localPosition = new Vector3(-centerToEyeDist, 0f, 0f);
-			offsetR.localPosition = new Vector3(centerToEyeDist, 0f, 0f);
+			float centerToEyeDist = _stereoSeparation;
+			offsetL.localPosition = new Vector3(-centerToEyeDist, 0f, -centerToEyeDist * 0.5f);
+			offsetR.localPosition = new Vector3(centerToEyeDist, 0f, -centerToEyeDist * 0.5f);
 		}
 
 		// Move the virtual head to its appropriate position relative to the opposite portal.
